c0efefe28c2058898b6dad05618009d7dab0eeb3
frkMaky <freakghost@gmail.com>
2021-01-18 21:12:58 +0100

    Practica Listas

---
+++ practica_listas.py
@@ -0,0 +1,12 @@
+
+miLista = ["María","Pepe","Marta","Antonio"]
+
+print(miLista[:])   #Imprime toda la lista
+
+print(miLista[-2])   #Imprime el elemento menos -2 de la lista (hacia atrás)
+
+print(miLista[2:])   #Imprime desde indice 2 (incluido) hasta el final 
+
+print(miLista[:2])   #Imprime desde el principio hasta el indice 2 (no incluido)
+
+
