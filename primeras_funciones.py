795644fbc40f00abc051415c60632ff2815e1c5e
Pablo A. Maquieira Varela <frkMaky@users.noreply.github.com>
2021-01-18 10:25:34 +0100

    Add files via upload

---
+++ primeras_funciones.py
@@ -0,0 +1,27 @@
+
+print("Hola Mundo") # Imprime por pantalla 
+
+print("Estamos aprendiendo instrucciones básivas") # Imprime por pantalla 
+
+print("Poco a poco iremos avanzando") # Imprime por pantalla 
+
+def mensaje():	
+	# Función que devuelve un mensaje
+	print("Mensaje dentro de la funcion")
+	print("Mensaje dentro de la funcion")
+	print("Mensaje dentro de la funcion")
+
+mensaje()	# Se llama a al funcion
+
+def suma(num1,num2):	
+	# Funcion que realiza la suma de 2 numeros pasados por argumentos
+	resultado = num1 + num2
+	return resultado
+
+print(suma(5,7)) # Se ejecuta la funcion (suma los valores pasados por argumentos)
+
+print(suma(2,3))
+
+print(suma(35,358))
+
+
