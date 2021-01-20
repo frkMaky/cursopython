00173b0c939be0638034e3d5c726bb1d1bf6bc58
frkMaky <freakghost@gmail.com>
2021-01-21 00:02:01 +0100

    practica_bucle_for

---
+++ practica_bucle_for.py
@@ -0,0 +1,16 @@
+# for variable in elemento_a_recorrer:
+
+for i in [1,2,3]:   # For con una lista
+    print("Lista: " + str(i) )
+
+for i in ["Primavera","Verano","Otoño","Invierno"]:   # For con una lista
+    print("Lista: " + str(i) )
+
+for i in (1,2,3):   # For con una Tupla
+    print("Tupla: " + str(i) )
+
+for i in ("Lunes","Martes","Miércoles"):   # For con una Tupla
+    print("Tupla: " + str(i) )
+
+for i in range(3):   # For con un rango
+    print("Rango: " + str(i+1) )
