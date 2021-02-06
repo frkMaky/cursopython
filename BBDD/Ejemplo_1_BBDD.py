436e8c8b9341783eb03cf4fc8b51446eb1a26d7d
frkMaky <freakghost@gmail.com>
2021-02-06 12:08:36 +0100

    BBDD_II

--- BBDD/Ejemplo_1_BBDD.py
+++ BBDD/Ejemplo_1_BBDD.py
@@ -6,9 +6,29 @@ miConexion = sqlite3.connect("PrimeraBase")	# Conectar / crear la BBDD
 
 miCursor = miConexion.cursor()	# Se crea el puntero / cursor
 
-# miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20) )")	# SQL
+# miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20) )")	# SQL CREAR TABLA
 
-miCursor.execute("INSERT INTO PRODUCTOS VALUES ('Balón', 15, 'Deportes')")	# SQL
+# miCursor.execute("INSERT INTO PRODUCTOS VALUES ('Balón', 15, 'Deportes')")	# SQL INSERTAR EN TABLA
+
+# Insertar varios
+variosProductos = [
+	
+	("Camiseta", 10, "Deportes"),
+	("Jarrón", 90, "Cerámica"),
+	("Camión", 20, "Juguetería"),
+
+]
+
+# miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", variosProductos) 	# Similar a consultas preparadas // Inserción con listas y tuplas
+
+# SQL LEER
+
+miCursor.execute("SELECT * FROM PRODUCTOS")
+
+variosProductosLeidos = miCursor.fetchall()
+
+for producto in variosProductosLeidos:
+	print(producto)
 
 # Siempre hay que verificar los cambios a realizar en BBDD
 
